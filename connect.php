c4c7b2c201ad14930665ead5c7e6c1f2c703239b
Kanta-sill <59639492+Kanta-sill@users.noreply.github.com>
2020-06-16 16:52:10 +0700

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,15 +1,25 @@
 <?php
-$ketnoi['host'] = 'ec2-52-44-55-63.compute-1.amazonaws.com';
-$ketnoi['dbname'] = 'devpq1sen6eh1j'; 
-$ketnoi['username'] = 'rjawmetmdmsgrc'; 
-$ketnoi['password'] = '5f40622ca0333f8ea7f0d5afd07053381dd612d3e8f55745d9bafd4f6e859556'; 
-@pg_connect(
-                "{$ketnoi['host']}", "{$ketnoi['username']}", "{$ketnoi['password']}")
-        or
-        die("Không thể kết nối database");
-@pg_select_db(
-                "{$ketnoi['dbname']}")
-        or
-        die("Không thể chọn database");
-?>
+$conn_string = "host=ec2-52-44-55-63.compute-1.amazonaws.com post=5432 dbname=devpq1sen6eh1j user= rjawmetmdmsgrc pass=5f40622ca0333f8ea7f0d5afd07053381dd612d3e8f55745d9bafd4f6e859556" 
+$dbconn=pg_connect(conn_string);
+if(isset(($_POST['username']))){
+	$username=$_POST['username'];
+}
+if(isset(($_POST['pass']))){
+	$pass=$_POST['pass'];
+}
 
+$sql="select * from tblaccount where _user='".$username."' and _pass='".$pass."'";
+
+$result = pg_query($dbconn, $sql);
+$row=pg_num_rows($result);
+if($row==1)
+{
+	echo "Log in success";
+}
+else
+{
+	echo "Log in fail";
+}
+
+
+?>
--- index.php
+++ index.php
@@ -1,21 +1,14 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-    <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<link rel="stylesheet" href="style.css"/>
+<head>
+	<meta charset="utf-8">
+	<title></title>
 </head>
 <body>
-    <form action='index.php' class="dangnhap" method='POST'>
-        Tên đăng nhập : <input type='text' name='username' />
-        Mật khẩu : <input type='password' name='password' />
-        <input type='submit' class="button" name="dangnhap" value='Đăng nhập' />
-        
-        <?php require 'function.php'; ?>
-    </form>
+	<form action="connect.php" method="POST" accept-charset="urf-8">
+		User: <input type="text" name="username" value="" placeholder="username">
+		Password: <input type="password" name="pass" value="" >
+		<input type="submit" name="" value="Log in">
+	</form>
 </body>
 </html>
