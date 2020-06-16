d76dda567841acaae791adbbf69de3aa2d844c09
Kanta-sill <59639492+Kanta-sill@users.noreply.github.com>
2020-06-16 16:02:50 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -6,12 +6,16 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        echo 'Hello';
-        ?>
-    </body>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link rel="stylesheet" href="style.css"/>
+</head>
+<body>
+    <form action='index.php' class="dangnhap" method='POST'>
+        Tên đăng nhập : <input type='text' name='username' />
+        Mật khẩu : <input type='password' name='password' />
+        <input type='submit' class="button" name="dangnhap" value='Đăng nhập' />
+        
+        <?php require 'function.php'; ?>
+    </form>
+</body>
 </html>
