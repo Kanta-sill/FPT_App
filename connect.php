323868476017b4e0ae5e9af983df0f965cb7d1f6
Kanta-sill <59639492+Kanta-sill@users.noreply.github.com>
2020-06-17 16:08:55 +0700

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$conn_string = "host=ec2-52-44-55-63.compute-1.amazonaws.com post=5432 dbname=devpq1sen6eh1j user=rjawmetmdmsgrc password=5f40622ca0333f8ea7f0d5afd07053381dd612d3e8f55745d9bafd4f6e859556";
+$conn_string = "host=ec2-52-44-55-63.compute-1.amazonaws.com port=5432 dbname=devpq1sen6eh1j user=rjawmetmdmsgrc password=5f40622ca0333f8ea7f0d5afd07053381dd612d3e8f55745d9bafd4f6e859556";
 $dbconn=pg_connect($conn_string);
 if(isset($_POST['username'])){
 	$username=$_POST['username'];
