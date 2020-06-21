b3954b556e95575761bab634c37de826b3e77a3c
Kanta-sill <59639492+Kanta-sill@users.noreply.github.com>
2020-06-21 20:56:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title></title>
+</head>
+<body>
+	<h1></h1>
+	<div class="container">
+		<div class="login">
+			<div class="form">
+				<form action="connect.php" method="post" accept-charset="urf-8">
+					<input type="text" name="username" placeholder="username">
+					<input type="password" name="pass" placeholder="password">
+					<button>Login</button>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+</body>
+</html>
